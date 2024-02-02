889a64cee491920a65e03c18f3a0ab9fcf023800
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-02-02 12:33:35 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Geant4 simulation framework of the NEXT experiment";
+
+  inputs = {
+    nixpkgs         .url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    nosys           .url = "github:divnix/nosys";
+  };
+
+  outputs = inputs @ {
+    nosys,
+    nixpkgs, # <---- This `nixpkgs` still has the `system` e.g. legacyPackages.${system}.zlib
+    ...
+  }:
+    let
+      outputs = import ./flake/outputs.nix;
+      system = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    in nosys (inputs // { inherit system; }) outputs;
+}
