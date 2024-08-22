897b0a605c67d562ffdb43b1cd5adf2cd52e6973
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-08-22 12:48:30 +0200

    Add scripts

---
+++ scan/run_script.sh
@@ -0,0 +1,14 @@
+
+script=$1
+folder=$2
+
+for p in 5.0 10.0 15.6; do
+  for el in 1 10; do
+    for dfh in 0 2 5; do
+      for dah in 2.5 5.0 10.0; do
+        echo "======== $p | $el | $dfh | $dah ========="
+        time python ${script} $folder/p_${p}_elgap_${el}_dfh_${dfh}_dah_${dah}*
+      done
+    done
+  done
+done
